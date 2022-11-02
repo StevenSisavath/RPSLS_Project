aa902dac5a0165957592eeb81aecd353c274e6c4
Sean <seanboateng2@gmail.com>
2022-11-02 16:11:24 -0400

    Provided winning condition for both ai versus ai

--- game.py
+++ game.py
@@ -33,9 +33,31 @@ Spock vaporizes Rock
     def determine_winner(self):
         if self.choose_number_of_players() == 0:
             self.ai_1 = self.ai.choose_options()
+            print(f'ai1 {self.ai_1} ')
             self.ai_2 = self.ai.choose_options()
-            if self.ai_1 == 'Rock' and self.ai_2 == 'Scissors':
-                print('AI 1 used rock to beat AI 2 with scissors!')
+            print(f'ai2 has {self.ai_2} ')
+            if self.ai_1 == 'Rock' and self.ai_2 == 'Scissors' or 'Lizard':
+                print('AI 1 beat AI_2')
+            elif self.ai_1 == 'Paper' and self.ai_2 == 'Rock' or 'Spock':
+                print('AI 1 beat AI_2')
+            elif self.ai_1 == 'Scissors' and self.ai_2 == 'Paper' or 'Lizard':
+                print('AI 1 beat AI_2')
+            elif self.ai_1 == 'Lizard' and self.ai_2 == 'Spock' or 'Paper':
+                print('AI 1 beat AI_2')
+            elif self.ai_1 == 'Spock' and self.ai_2 == 'Scissors' or 'Rock':
+                print('AI 1 beat AI_2')
+
+            elif self.ai_2 == 'Rock' and self.ai_1 == 'Scissors' or 'Lizard':
+                print('AI 2 beat AI_1')
+            elif self.ai_2 == 'Paper' and self.ai_1 == 'Rock' or 'Spock':
+                print('AI 2 beat AI_1')
+            elif self.ai_2 == 'Scissors' and self.ai_1 == 'Paper' or 'Lizard':
+                print('AI 2 beat AI_1')
+            elif self.ai_2 == 'Lizard' and self.ai_1 == 'Spock' or 'Paper':
+                print('AI 2 beat AI_1')
+            elif self.ai_2 == 'Spock' and self.ai_1 == 'Scissors' or 'Rock':
+                print('AI 2 beat AI_1')
+
 
     def run_game(self):
         self.display_welcome()
